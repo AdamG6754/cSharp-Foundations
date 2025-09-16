06fe517e2e7f735edf1b276503bbbecc5ca18ab3
AdamG6754 <adam.goltsman@gmail.com>
2025-09-16 09:01:46 -0400

    minor C# practice

--- cSharpFoundation/cSharpFoundation/Program.cs
+++ cSharpFoundation/cSharpFoundation/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Suck My Balls, World!");
+
+
+
+var myName = "Adam";
+string myNameVar = "Adam";
+int myFavNum = 22;
+
+
+
+
+Console.WriteLine(myName + " " +  myNameVar   + " " + myFavNum);
