7e1c6372141869e20538de6cd6f4054bc6b362bc
AdamG6754 <adam.goltsman@gmail.com>
2025-09-17 16:35:50 -0400

    completed the assignment

--- cSharpFoundation/cSharpFoundation/Program.cs
+++ cSharpFoundation/cSharpFoundation/Program.cs
@@ -1,13 +1,83 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Suck My Balls, World!");
+﻿OperatorExamples();
+
+void OperatorExamples()
+{
+    //declare width & set to 3
+    int width = 3;
+
+    //add 1
+    width++;
+
+    //declare more variables
+    int height = 2 + 4;
+    int area = width * height;
+    Console.WriteLine(area);
+
+
+    while (area < 50)
+    {
+        height++;
+        area = width * height;
+    }
+    do
+    {
+        width--;
+        area = width * height;
+    } while (area > 25);
+
+    //next statements declare string variables
+    string result = "The area";
+    result = result + " is " + area;
+    Console.WriteLine(result);
+
+    //boolean value:
+    bool truthVal = true;
+    Console.WriteLine(truthVal);
+}
 
 
 
-var myName = "Adam";
-string myNameVar = "Adam";
-int myFavNum = 22;
+TryAnIf();
+TrySomeLoops();
+TryAnIfElse();
 
+void TryAnIf()
+{
+    int someValue = 4;
+    string name = "Bobbo Jr.";
+    if ((someValue == 3) && (name == "Joe"))
+    {
+        Console.WriteLine("x is 3 and the name is Joe");
+    }
+    Console.WriteLine("this line runs no matter what");
+}
 
+void TryAnIfElse()
+{
+    int x = 5;
+    if (x == 10)
+    {
+        Console.WriteLine("x must be 10");
+    }
+    else
+    {
+        Console.WriteLine("x isn’t 10");
+    }
+}
 
+void TrySomeLoops()
+{
+    int count = 0;
 
-Console.WriteLine(myName + " " +  myNameVar   + " " + myFavNum);
+    while (count < 10)
+    {
+        count = count + 1;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        count = count - 1;
+    }
+
+    Console.WriteLine("The answer is " + count);
+}
